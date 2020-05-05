1503b6e06cab5912fb7452888e6120dcf7ee221f
sarah elhwarii <sarah.elhwarii92@gmail.com>
2020-05-06 01:33:06 +0300

    modifying listening to keypresses

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const drumsNo = document.querySelectorAll(".drum").length;
 for (let i = 0; i < drumsNo; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     const drumName = this.classList[0];
-    const audio = new Audio(`sounds/${drumName}.mp3`);
-    audio.play();
+    makeSound(drumName);
   });
 }
 
@@ -15,8 +14,12 @@ document.addEventListener("keydown", function (e) {
     const keyTest = document.querySelectorAll(".drum")[i].contains(drumKey);
     if (keyTest) {
       const selected = document.querySelector(".drumKey").classList[0];
-      const audio = new Audio(`sounds/${selected}.mps`);
-      audio.play();
+      makeSound(selected);
     }
   }
 });
+
+function makeSound(sound) {
+  const audio = new Audio(`sounds/${sound}.mp3`);
+  audio.play();
+}
